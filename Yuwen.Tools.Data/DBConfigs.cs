9ca515905900124118dc6d0f7656a7af7cae8ffb
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-09-01 07:40:44 +0000

    让数据库读取能够从配置文件中读取

--- Yuwen.Tools.Data/DBConfigs.cs
+++ Yuwen.Tools.Data/DBConfigs.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
 
 namespace Yuwen.Tools.Data
 {
@@ -12,12 +13,15 @@ namespace Yuwen.Tools.Data
         /// <returns></returns>
         public static string GetDBConnectString()
         {
-            return string.Format("Data Source={0}\\config\\db.config", System.Web.HttpContext.Current.Server.MapPath("~/"));
+            //return string.Format("Data Source={0}\\config\\db.config", System.Web.HttpContext.Current.Server.MapPath("~/"));
+            //return @"Data Source=D:\database\sqlite\aspx163.config";
+            return ConfigurationSettings.AppSettings["dbconnstring"];
         }
 
         public static string GetDbType()
         {
-            return "Sqlite";
+            //return "Sqlite";
+            return ConfigurationSettings.AppSettings["dbtype"];
         }
     }
 }
