f0e8374a7ecfed7c15b66d5d1a81570506e72e55
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-12-03 14:29:56 +0000

    增加用户处理类和数据方法.

---
+++ iTCA.Yuwen.Data/IUsers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using iTCA.Yuwen.Entity;
+
+namespace iTCA.Yuwen.Data
+{
+    public partial interface IDataProvider
+    {
+        /// <summary>
+        /// 取得用户信息(用于登录)
+        /// </summary>
+        /// <param name="loginid">登录id(UserName或者Email)</param>
+        /// <param name="password">密码</param>
+        /// <param name="logintype">登录类型(0为邮箱地址登录,1为UserName登录)</param>
+        /// <returns>用户记录</returns>
+        IDataReader GetUserInfo(string loginid, string password, int logintype);
+        IDataReader GetUserInfo(int uid);
+        IDataReader GetUserInfo(string username);
+    }
+}
