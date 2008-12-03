18ac9f91023416b7098f4d2e93d0d88537add945
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-12-03 05:47:39 +0000

    分页调整

--- trunk/iTCA.Yuwen.Web/index.aspx.cs
+++ trunk/iTCA.Yuwen.Web/index.aspx.cs
@@ -15,12 +15,19 @@ namespace iTCA.Yuwen.Web
         /// 协会公告列表
         /// </summary>
         protected List<ArticleInfo> annlist;
-
+        /// <summary>
+        /// 推荐文章页码
+        /// </summary>
+        protected int pagecount;
+        protected string pagecounthtml;
         protected override void Page_Show()
         {
             pagetitle = "欢迎来到 iTCA 重庆工学院计算机协会 - iTCA 重庆工学院计算机协会";
             annlist = Articles.GetArticleCollection(1, 12, 1);
             newslist = Articles.GetArticleCollection("2,3,4", 5, 1);
+
+            pagecount = Articles.GetArticleCollectionPageCount(1, 12);
+            pagecounthtml = Natsuhime.Utils.GetStaticPageNumbersHtml(1, pagecount, string.Format("showcolumn-{0}", 1), ".aspx", 8);
         }
     }
 }
