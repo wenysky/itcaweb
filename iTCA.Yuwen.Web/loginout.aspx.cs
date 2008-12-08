a25d0254590c787f60618972da15dda0b35b40d8
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-12-08 09:46:41 +0000

    完成登录注销,增加注册页面

---
+++ iTCA.Yuwen.Web/loginout.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace iTCA.Yuwen.Web
+{
+    public partial class loginout : BasePage
+    {
+        protected override void Page_Show()
+        {
+            System.Web.HttpContext.Current.Response.Cookies["cmsnt"].Expires = DateTime.Now.AddDays(-1d);
+        }
+    }
+}
---
+++ iTCA.Yuwen.Web/register.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using iTCA.Yuwen.Core;
+using iTCA.Yuwen.Entity;
+
+namespace iTCA.Yuwen.Web
+{
+    public partial class register : BasePage
+    {
+        protected override void Page_Show()
+        {
+            UserInfo info = new UserInfo();
+            info.Adminid = 1;
+            info.Articlecount = 0;
+            info.Bdday = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            info.Del = 0;
+            info.Email = "admin@qq.com";
+            info.Groupid = 1;
+            info.Hi = "";
+            info.Lastlogdate = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            info.Lastlogip = "127.0.0.1";
+            info.Msn = "";
+            info.Nickname = "管理员";
+            info.Password = "123";
+            info.Qq = "";
+            info.Realname = "";
+            info.Regdate = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            info.Regip = "0.0.0.0";
+            info.Replycount = 0;
+            info.Topiccount = 0;
+            info.Username = "admin";
+
+            Users.AddUser(info);
+                
+        }
+    }
+}
