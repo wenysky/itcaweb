2ab15185fda8ede63e1071d3792e92a19f0cb6af
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-12-08 09:46:41 +0000

    完成登录注销,增加注册页面

--- trunk/iTCA.Yuwen.Web/login.aspx.cs
+++ trunk/iTCA.Yuwen.Web/login.aspx.cs
@@ -8,23 +8,41 @@ namespace iTCA.Yuwen.Web
     {
         protected override void Page_Show()
         {
-            if (System.Web.HttpContext.Current.Request.Form["loginid"] != null && System.Web.HttpContext.Current.Request.Form["password"] != null)
+            if (System.Web.HttpContext.Current.Request.HttpMethod.Equals("POST"))
             {
-                string loginid = System.Web.HttpContext.Current.Request.Form["loginid"].ToString();
-                string password = System.Web.HttpContext.Current.Request.Form["password"].ToString();
-                UserInfo info = Users.GetUserInfo(loginid, password, 0);
-                if (info != null)
+                if (System.Web.HttpContext.Current.Request.Form["loginid"] != null && System.Web.HttpContext.Current.Request.Form["password"] != null)
                 {
-                    System.Web.HttpContext.Current.Response.Write("login success!");
+                    string loginid = System.Web.HttpContext.Current.Request.Form["loginid"].ToString();
+                    string password = System.Web.HttpContext.Current.Request.Form["password"].ToString();
+                    UserInfo info = Users.GetUserInfo(loginid, password, 0);
+                    if (info != null)
+                    {
+                        System.Web.HttpCookie cookie = new System.Web.HttpCookie("cmsnt");
+                        cookie.Values["userid"] = info.Uid.ToString();
+                        cookie.Values["password"] = info.Password;
+
+
+                        cookie.Expires = DateTime.Now.AddDays(30d);
+                        
+                        //if (cookieDomain != string.Empty && HttpContext.Current.Request.Url.Host.IndexOf(cookieDomain) > -1 && IsValidDomain(HttpContext.Current.Request.Url.Host))
+                        //{
+                        //    cookie.Domain = cookieDomain;
+                        //}
+
+                        System.Web.HttpContext.Current.Response.AppendCookie(cookie);
+                        System.Web.HttpContext.Current.Response.Write("login success!");
+                        System.Web.HttpContext.Current.Response.Redirect("usercontrolpanl.aspx");
+                    }
+                    else
+                    {
+                        System.Web.HttpContext.Current.Response.Cookies["cmsnt"].Expires = DateTime.Now.AddDays(-1d);
+                        System.Web.HttpContext.Current.Response.Write("login failed");
+                    }
                 }
                 else
                 {
-                    System.Web.HttpContext.Current.Response.Write("login failed");
+                    System.Web.HttpContext.Current.Response.Write("用户名或密码为空");
                 }
-            }
-            else
-            {
-                System.Web.HttpContext.Current.Response.Write("用户名或密码为空");
             }
         }
     }
