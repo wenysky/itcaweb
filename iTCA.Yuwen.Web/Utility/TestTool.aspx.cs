1887503348468d4186b1f5730ddd3f8b43077fc7
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-12-12 07:42:41 +0000

    测试程序

---
+++ iTCA.Yuwen.Web/Utility/TestTool.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using iTCA.Yuwen.Config;
+using Natsuhime;
+using Natsuhime.Common;
+
+namespace iTCA.Yuwen.Web.Utility
+{
+    public partial class TestTool : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            BaseConfigInfo info = new BaseConfigInfo();
+            info.Dbconnectstring = @"Data Source=F:\Doctments\Works\Mine\iTCAWeb\trunk\iTCA.Yuwen.Web\config\db.config";
+            info.Dbtype = "Sqlite";
+            info.Tableprefix = "wy_";
+            info.ApplictionPath = "/";
+            SerializationHelper.SaveXml(info, Utils.GetMapPath("~/hmsm.config"));
+        }
+    }
+}
