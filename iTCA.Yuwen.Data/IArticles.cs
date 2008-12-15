138a660916d7e8a801c8d5c5cd672517858b7555
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-12-15 10:25:29 +0000

    整理代码,把column和article分成2个文件

--- iTCA.Yuwen.Data/IArticles.cs
+++ iTCA.Yuwen.Data/IArticles.cs
@@ -7,28 +7,20 @@ namespace iTCA.Yuwen.Data
 {
     public partial interface IDataProvider
     {
+        #region 取得列表
         IDataReader GetArticles(int cid, int pagesize, int currentpage);
-        IDataReader GetUserArticles(int uid, int pagesize, int currentpage);
-        IDataReader GetArticles(string cids, int pagesize, int currentpage);
         int GetArticleCollectionPageCount(int cid, int pagesize);
-        int GetUserArticleCollectionPageCount(int uid, int pagesize);
-        int GetArticleCollectionPageCount(string cids, int pagesize);
 
-        IDataReader GetArticleColumnList();
+        IDataReader GetUserArticles(int uid, int pagesize, int currentpage);
+        int GetUserArticleCollectionPageCount(int uid, int pagesize);
+
+        IDataReader GetArticles(string cids, int pagesize, int currentpage);
+        int GetArticleCollectionPageCount(string cids, int pagesize);
+        #endregion
 
         IDataReader GetArticleInfo(int articleid);
-
         void CreateArticle(ArticleInfo articleinfo);
-
         void EditArticle(ArticleInfo articleinfo);
-
         void DeleteArticle(int articleid);
-
-        void CreateColumn(ColumnInfo columninfo);
-
-        void DeleteColumn(int columnid);
-
-        void EditColumn(ColumnInfo columninfo);
-
     }
 }
