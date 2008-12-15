193a9fbfebcf45bd011c7bc9400638c577b6666b
wenysky <wenysky@58c460e3-cb50-0410-ad61-4d056c5f5e91>
2008-12-15 08:28:01 +0000

    完成评论模块

---
+++ trunk/iTCA.Yuwen.Data/IComments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using iTCA.Yuwen.Entity;
+
+namespace iTCA.Yuwen.Data
+{
+    public partial interface IDataProvider
+    {
+        #region 取得列表
+        IDataReader GetComments(int articleid, int pagesize, int currentpage);
+        IDataReader GetUserComments(int uid, int pagesize, int currentpage);
+        int GetCommentsPageCount(int articleid, int pagesize);
+        int GetUserCommentsPageCount(int uid, int pagesize);
+        #endregion
+
+        IDataReader GetCommentInfo(int commentid);
+
+        void CreateComment(CommentInfo commentinfo);
+        void DeleteComment(int commentid);
+        void GradeComment(int commentid, int type);
+    }
+}
---
+++ trunk/iTCA.Yuwen.Web/comment.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using iTCA.Yuwen.Core;
+using iTCA.Yuwen.Entity;
+using Natsuhime.Web;
+
+namespace iTCA.Yuwen.Web
+{
+    public partial class comment : BasePage
+    {
+        protected override void Page_Show()
+        {
+            if (userinfo == null)
+            {
+                currentcontext.Response.Write("请登录后再留言评论.");
+                currentcontext.Response.End();
+            }
+            string action = YRequest.GetQueryString("action");
+            if (action == string.Empty)
+            {
+                currentcontext.Response.End();
+            }
+            if (action == "postcomment")
+            {
+                string content = YRequest.GetFormString("commentcontent");
+                int articleid = YRequest.GetQueryInt("articleid", 0);
+                if (content != string.Empty && articleid > 0)
+                {
+                    if (content != string.Empty)
+                    {
+                        CommentInfo info = new CommentInfo();
+                        info.Articleid = articleid;
+                        info.Uid = userinfo.Uid;
+                        info.Username = userinfo.Username;
+                        info.Postdate = DateTime.Now.ToString();
+                        info.Del = 0;
+                        info.Content = Utils.RemoveUnsafeHtml(content);
+                        info.Goodcount = 0;
+                        info.Badcount = 0;
+                        Comments.CreateComment(info);
+                        currentcontext.Response.Redirect(YRequest.GetUrlReferrer());
+                    }
+                }
+                else
+                {
+                    currentcontext.Response.Write("参数为空.");
+                    currentcontext.Response.End();
+                }
+            }
+            else if (action == "grade")
+            {
+                int commentid = YRequest.GetQueryInt("commentid", 0);
+                if (commentid > 0)
+                {
+                    int type = YRequest.GetQueryInt("type", 0);
+                    Comments.GradeComment(commentid, type);
+                    currentcontext.Response.Redirect(YRequest.GetUrlReferrer());
+                }
+                else
+                {
+                    currentcontext.Response.Write("参数为空.");
+                    currentcontext.Response.End();
+                }
+            }
+            else
+            {
+                currentcontext.Response.Write("err");
+                currentcontext.Response.End();
+            }
+        }
+    }
+}
